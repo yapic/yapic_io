more effective tile position computation in training batch and fix dimensionorder in prediction batch

yapic_io/tests/test_prediction_batch.py
@@ -3,9 +3,12 @@ from unittest import TestCase
 import os
 from yapic_io.connector import io_connector
 import numpy as np
-from numpy.testing import assert_array_almost_equal
+from numpy.testing import assert_array_almost_equal, assert_array_equal
 from yapic_io import TiffConnector, Dataset, PredictionBatch
 from bigtiff import Tiff
+from yapic_io.ilastik_connector import IlastikConnector
+
+
 base_path = os.path.dirname(__file__)
 
 
@@ -25,8 +28,10 @@ class TestPredictionBatch(TestCase):
         p = PredictionBatch(d, batch_size, size)
 
         self.assertEqual(len(p._all_tile_positions), 6 * 4 * 3)
-        self.assertEqual(len(p._all_tile_positions),
-                         len(set(p._all_tile_positions)))
+        tilepos = [(p[0], tuple(p[1])) for p in p._all_tile_positions]
+
+        self.assertEqual(len(tilepos),
+                         len(set(tilepos)))
 
     def test_computepos_2(self):
         img_path = os.path.join(
@@ -41,7 +46,11 @@ class TestPredictionBatch(TestCase):
         batch_size = 1
 
         p = PredictionBatch(d, batch_size, size)
-        self.assertEqual(p._all_tile_positions, [(0, (0, 0, 0))])
+
+        val = [(0, (0, 0, 0))]
+        for pos, valpos in zip(p._all_tile_positions, val):
+            assert_array_equal(pos[1], np.array(valpos[1]))
+            self.assertEqual(pos[0], valpos[0])
 
     def test_computepos_3(self):
         img_path = os.path.join(
@@ -56,8 +65,11 @@ class TestPredictionBatch(TestCase):
         batch_size = 1
 
         p = PredictionBatch(d, batch_size, size)
-        self.assertEqual(p._all_tile_positions,
-                         [(0, (0, 0, 0)), (0, (1, 0, 0))])
+
+        val = [(0, (0, 0, 0)), (0, (1, 0, 0))]
+        for pos, valpos in zip(p._all_tile_positions, val):
+            assert_array_equal(pos[1], np.array(valpos[1]))
+            self.assertEqual(pos[0], valpos[0])
 
     def test_getitem_1(self):
         img_path = os.path.join(
@@ -113,10 +125,17 @@ class TestPredictionBatch(TestCase):
         batch_size = 2
         p = PredictionBatch(d, batch_size, size)
 
-        self.assertEqual(p[0].current_tile_positions,
-                         [(0, (0, 0, 0)), (0, (1, 0, 0))])
-        self.assertEqual(p[1].current_tile_positions,
-                         [(0, (2, 0, 0))])
+        val = [(0, (0, 0, 0)), (0, (1, 0, 0))]
+        for pos, valpos in zip(p[0].current_tile_positions, val):
+            assert_array_equal(pos[1], np.array(valpos[1]))
+            self.assertEqual(pos[0], valpos[0])
+
+        val = [(0, (2, 0, 0))]
+        for pos, valpos in zip(p[1].current_tile_positions, val):
+            assert_array_equal(pos[1], np.array(valpos[1]))
+            self.assertEqual(pos[0], valpos[0])
+
+
 
     def test_put_probmap_data(self):
         img_path = os.path.join(
@@ -252,6 +271,49 @@ class TestPredictionBatch(TestCase):
 
         data = np.ones((1, 6, 1, 3, 4))
         p[0].put_probmap_data(data)
+
+
+    def test_put_probmap_data_dimorder_zxyc(self):
+
+        img_path = os.path.join(
+            base_path,
+            '../test_data/ilastik/dimensionstest/images/*')
+        label_path = os.path.join(
+            base_path,
+            '../test_data/ilastik/dimensionstest/x15_y10_z2_c4_classes2.ilp')
+
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            c = TiffConnector(img_path, 'some/path', savepath=tmpdirname)
+            d = Dataset(c)
+
+            size = (2, 3, 5) # zxy
+            batch_size = 1
+
+            # shape: 1 batch, 4 channels, 2 z, 3 x, 5 y
+
+            p = PredictionBatch(d, batch_size, size)
+            self.assertEqual((1, 4, 2, 3, 5), p[0].pixels().shape)
+            self.assertEqual((1, 4, 2, 3, 5), p[1].pixels().shape)
+
+            p.set_pixel_dimension_order('bzxyc')
+
+            self.assertEqual((1, 2, 3, 5, 4), p[0].pixels().shape)
+            self.assertEqual((1, 2, 3, 5, 4), p[1].pixels().shape)
+
+            pixels = p[0].pixels()
+
+            for tile in p:
+                tile.put_probmap_data(pixels)
+
+
+            # assert False
+
+
+
+
+
+
+
 
     def test_prediction_loop(self):
         with tempfile.TemporaryDirectory() as tmpdirname:

yapic_io/tests/test_utils.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 import yapic_io.utils as ut
 import numpy as np
+from numpy.testing import assert_array_equal
 
 class TestUtils(TestCase):
 
@@ -9,57 +10,68 @@ class TestUtils(TestCase):
         size = (5, 2)
 
         res = ut.compute_pos(shape, size)
-        val = [(0, 0), (0, 2), (0, 3), (3, 0), (3, 2), (3, 3)]
+        print(res)
+        val = np.array([(0, 0), (0, 2), (0, 3), (3, 0), (3, 2), (3, 3)])
 
-        self.assertEqual(res, val)
+        assert_array_equal(res, val)
 
-        res = ut.compute_pos(shape, size, sliding_window=True)
-        val = [(0, 0), (1, 0), (2, 0), (3, 0),
-               (0, 1), (1, 1), (2, 1), (3, 1),
-               (0, 2), (1, 2), (2, 2), (3, 2),
-               (0, 3), (1, 3), (2, 3), (3, 3)]
+        res = ut.compute_pos(shape, size, sliding=(1, 1))
+        val = np.array([(0, 0), (1, 0), (2, 0), (3, 0),
+                        (0, 1), (1, 1), (2, 1), (3, 1),
+                        (0, 2), (1, 2), (2, 2), (3, 2),
+                        (0, 3), (1, 3), (2, 3), (3, 3)])
 
-        self.assertEqual(res, val)
+        assert_array_equal(res, val)
 
     def test_compute_pos_2(self):
         shape = (6, 4)
         size = (2, 2)
 
         res = ut.compute_pos(shape, size)
-        val = [(0, 0), (0, 2), (2, 0), (2, 2), (4, 0), (4, 2)]
-        self.assertEqual(val, res)
+        val = np.array([(0, 0), (0, 2), (2, 0), (2, 2), (4, 0), (4, 2)])
+        assert_array_equal(val, res)
 
-        res = ut.compute_pos(shape, size, sliding_window=True)
-        val = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0),
-               (0, 1), (1, 1), (2, 1), (3, 1), (4, 1),
-               (0, 2), (1, 2), (2, 2), (3, 2), (4, 2)]
-        self.assertEqual(val, res)
+        res = ut.compute_pos(shape, size, sliding=(1, 1))
+        val = np.array([(0, 0), (1, 0), (2, 0), (3, 0), (4, 0),
+                        (0, 1), (1, 1), (2, 1), (3, 1), (4, 1),
+                        (0, 2), (1, 2), (2, 2), (3, 2), (4, 2)])
+        assert_array_equal(val, res)
+
+        res = ut.compute_pos(shape, size, sliding=(2, 2))
+        val = np.array([(0, 0), (2, 0), (4, 0),
+                        (0, 2), (2, 2),  (4, 2)])
+        assert_array_equal(val, res)
+
+        res = ut.compute_pos(shape, size, sliding=(1, 2))
+        val = np.array([(0, 0), (1, 0), (2, 0), (3, 0), (4, 0),
+                        (0, 2), (1, 2), (2, 2), (3, 2), (4, 2)])
+        assert_array_equal(val, res)
 
     def test_compute_pos_3d(self):
 
         shape = (2, 4, 3)
         size = (2, 4, 3)
 
-        val = [(0, 0, 0)]
+        val = np.array([(0, 0, 0)])
 
         res = ut.compute_pos(shape, size)
-        self.assertEqual(val, res)
+        assert_array_equal(val, res)
 
-        res = ut.compute_pos(shape, size, sliding_window=True)
-        self.assertEqual(val, res)
+        res = ut.compute_pos(shape, size, sliding=(1, 1, 1))
+        assert_array_equal(val, res)
 
     def test_compute_pos_3d_2(self):
 
         shape = (2, 4, 3)
         size = (1, 4, 3)
 
-        val = [(0, 0, 0), (1, 0, 0)]
+        val = np.array([(0, 0, 0), (1, 0, 0)])
 
         res = ut.compute_pos(shape, size)
-        self.assertEqual(val, res)
+        assert_array_equal(val, res)
 
-        res = ut.compute_pos(shape, size, sliding_window=True)
-        self.assertEqual(val, res)
+        res = ut.compute_pos(shape, size, sliding=(1, 1, 1))
+        assert_array_equal(val, res)
 
 
     def test_compute_pos_3d_3(self):
@@ -67,15 +79,15 @@ class TestUtils(TestCase):
         shape = (6, 4, 3)
         size = (2, 4, 3)
 
-        val = [(0, 0, 0), (2, 0, 0), (4, 0, 0)]
+        val = np.array([(0, 0, 0), (2, 0, 0), (4, 0, 0)])
 
         res = ut.compute_pos(shape, size)
-        self.assertEqual(val, res)
+        assert_array_equal(val, res)
 
-        val = [(0, 0, 0), (1, 0, 0), (2, 0, 0), (3, 0, 0), (4, 0, 0)]
+        val = np.array([(0, 0, 0), (1, 0, 0), (2, 0, 0), (3, 0, 0), (4, 0, 0)])
 
-        res = ut.compute_pos(shape, size, sliding_window=True)
-        self.assertEqual(val, res)
+        res = ut.compute_pos(shape, size, sliding=(1, 1, 1))
+        assert_array_equal(val, res)
 
     def test_compute_pos_large_tile(self):
 
@@ -86,7 +98,7 @@ class TestUtils(TestCase):
 
         print(res)
 
-        res = ut.compute_pos(shape, size, sliding_window=True)
+        res = ut.compute_pos(shape, size, sliding=(1, 1))
 
     def test_remove_overlapping_pos(self):
 

yapic_io/training_batch.py
@@ -4,6 +4,7 @@ from yapic_io.minibatch import Minibatch
 from yapic_io.utils import compute_pos, find_overlapping_tiles
 import logging
 import os
+from functools import lru_cache
 
 logger = logging.getLogger(os.path.basename(__file__))
 logger.setLevel(logging.INFO)
@@ -49,6 +50,15 @@ class TrainingBatch(Minibatch):
     >>>
     >>> c = TiffConnector(pixel_img_dir, label_img_dir, savepath=savepath.name)
     >>> m = TrainingBatch(Dataset(c), tile_size, padding_zxy=padding)
+    compute pos for image 0 of 3
+    compute pos for image 1 of 3
+    compute pos for image 2 of 3
+    compute pos for image 0 of 3
+    compute pos for image 1 of 3
+    compute pos for image 2 of 3
+    compute pos for image 0 of 3
+    compute pos for image 1 of 3
+    compute pos for image 2 of 3
     >>> print(m)
     TrainingBatch (batch_size: 3, tile_size (zxy): (1, 5, 4), augment: {'flip'}
     >>>
@@ -87,7 +97,7 @@ class TrainingBatch(Minibatch):
         self._pixels = None
         self._weights = None
 
-        self.tile_pos_for_label = {key: self.tile_positions()
+        self.tile_pos_for_label = {key: self.tile_positions(sliding=True)
                                    for key in self.labels}
 
     def __repr__(self):
@@ -178,6 +188,7 @@ class TrainingBatch(Minibatch):
         return np.moveaxis(self._weights, [0, 1, 2, 3, 4],
                            self.pixel_dimension_order)
 
+    # @lru_cache(maxsize=10)
     def tile_positions(self, sliding=True):
         '''
         Get all possible sliding window tile positions of the dataset.
@@ -195,12 +206,23 @@ class TrainingBatch(Minibatch):
         '''
 
         tile_pos = []
+
+        # compute shift of sliding window
+        if sliding:
+            shifted_zxy = np.ceil(np.array(self.tile_size_zxy)/3).astype('int')
+            # overlap by one third
+        else:
+            shifted_zxy = None  # no overlap
+
         for i in range(self.dataset.n_images):
+            print('compute pos for image {} of {}'.format(
+                                        i,
+                                        self.dataset.n_images))
             img_shape = self.dataset.image_dimensions(i)[1:]
             pos = [(i, p[0], p[1], p[2])
                    for p in compute_pos(img_shape,
                                         self.tile_size_zxy,
-                                        sliding_window=sliding)]
+                                        sliding=shifted_zxy)]
             tile_pos += pos
 
         return tile_pos
@@ -358,11 +380,17 @@ class TrainingBatch(Minibatch):
         '''
 
         # random pollng loop
-        for counter in range(100):
-
+        counter = 0
+        while counter <= len(self.tile_pos_for_label[for_label]):
+            counter += 1
             pos = self.tile_pos_for_label[for_label]
-            choice = np.random.choice(range(len(pos)))
+
+            msg = 'no label data for label {} in dataset'.format(for_label)
+            assert len(pos) > 0, msg
+
+            choice = np.random.randint(0, len(pos))
             pos_selected = pos[choice]
+
             image_nr = pos_selected[0]
             pos_zxy = pos_selected[1:]
 
@@ -382,15 +410,17 @@ class TrainingBatch(Minibatch):
                        'labelvalue {}').format(counter,
                                                for_label)
                 logger.info(msg)
+
                 return tile_data
 
             else:
                 # remove tile position for the label, since no labels here
-                self.tile_pos_for_label[for_label].pop(choice)
+                r = self.tile_pos_for_label[for_label].pop(choice)
 
         msg = ('Could not fetch random tile containing labelvalue {} ' +
                'within {} trials').format(for_label, counter)
         logger.warning(msg)
+
         return tile_data
 
 

yapic_io/utils.py
@@ -57,7 +57,7 @@ def assert_valid_image_subset(image_shape, pos, size):
     assert not (image_shape < (pos + size)).any(), 'tile out of image bounds'
 
 
-def compute_pos(img_shape, tile_shape, sliding_window=False):
+def compute_pos(img_shape, tile_shape, sliding=None):
 
     '''
     Computes all  positions for fetching tiles of a given size (tile
@@ -73,19 +73,24 @@ def compute_pos(img_shape, tile_shape, sliding_window=False):
         Image shape.
     tile_shape : tuple of size ndim
         Tile shape.
-    sliding_window : boolean
-        If True, positions are calculated for overlapping tiles. The tile
-        positions are shifted by one pixel in each direction.
-        If False, positions are calculated for non overlapping tiles. However
+    sliding : None or tuple
+        if tuple, positions are calculated for overlapping tiles. The tile
+        positions are shifted by  n pixels in each direction as defined in the
+        tuple.
+        If None, positions are calculated for non overlapping tiles. However
         the last tile could overlap with its neighbors.
+    shifted : integer
+        Nr of pixels to shift the sliding window in all dimensions.
 
     Returns
     -------
     array_like
         list of n-dimensional positions
     '''
-    img_shape = np.array(img_shape)
-    tile_shape = np.array(tile_shape)
+    img_shape = np.asarray(img_shape)
+    tile_shape = np.asarray(tile_shape)
+
+    assert sliding is None or len(sliding) == len(img_shape)
 
     msg = 'tile size {} > image shape {}'.format(tile_shape, img_shape)
     assert not (tile_shape > img_shape).any(), msg
@@ -102,21 +107,22 @@ def compute_pos(img_shape, tile_shape, sliding_window=False):
 
     pos = list(itertools.product(*pos_per_dim))
 
-    if not sliding_window or len(pos) <= 1:
+    if sliding is None or len(pos) <= 1:
         # non overlapping tile positions
-        return pos
+        return np.array(pos)
 
     # if sliding window: interpolate missing positions with mgrid
     n_dims = len(img_shape)
 
-    slices = [slice(pos[0][i], pos[-1][i]+1) for i in range(n_dims)]
+    slices = [slice(pos[0][i], pos[-1][i]+1, s)
+              for s, i in zip(sliding, range(n_dims))]
     mesh = np.mgrid[slices]
     mesh = np.array(mesh).swapaxes(0, -1)
 
     n_dims = len(img_shape)
     pos_array = mesh.reshape((np.int(mesh.size/n_dims), n_dims))
 
-    return [tuple(e) for e in pos_array]
+    return pos_array
 
 
 def find_overlapping_tiles(a, pos, shape):