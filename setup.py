2025406443680166d9fd336036e87fc74c4e8890
cmohl2013 <cmohl@yahoo.com>
2020-04-16 17:12:22 +0200

    updated pyilastik requirement version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 reqs = ['numpy>=1.13.1',
         'munkres>=1.0.8',
         'scikit_image>=0.12.3',
-        'pyilastik>=0.0.5',
+        'pyilastik>=0.0.7',
         'bigtiff>=0.1.2']
 
 
