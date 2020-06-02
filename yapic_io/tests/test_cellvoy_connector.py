d3e42de58353d3318e042a54d457d9d52dbd9281
Christoph Moehl <cmohl@yahoo.com>
2020-06-02 16:50:20 +0200

    added support for cellvoyager datasets (#14)
    * wip added CellvoyConnector class to bind raw cellvoyager imaging data
    
    * finalized cellvoy connector class
    
    * modified cellvoy test
    
    * fixed ordering problem of cellvoy filenames
    
    * modified cellvoy test
    
    * pep8 issues fixed

---
+++ yapic_io/tests/test_cellvoy_connector.py
@@ -0,0 +1,53 @@
+import itertools
+from unittest import TestCase
+import os
+import numpy as np
+from numpy.testing import assert_array_equal
+from yapic_io.tiff_connector import TiffConnector
+from yapic_io.cellvoy_connector import CellvoyConnector
+import yapic_io.cellvoy_connector as cv
+import logging
+import tempfile
+from pathlib import Path
+logger = logging.getLogger(os.path.basename(__file__))
+
+base_path = os.path.dirname(__file__)
+data_dir = os.path.join(base_path, '../test_data/cellvoyager')
+
+
+class TestCellvoyConnector(TestCase):
+
+    def test_compare_cellvoyconnector_with_tiffconnector(self):
+
+        img_files_multi = [os.path.join(
+            data_dir,
+            '../tif_images/GASPR01S01R01p01E01CD_A05_T0001F001_merge.tif')]
+
+        cc = CellvoyConnector(data_dir, 'some/path')
+        tc = TiffConnector(img_files_multi, 'some/path')
+
+        assert_array_equal(tc.image_dimensions(0), cc.image_dimensions(1))
+
+        pxt = tc.get_tile(0, (3, 0, 0, 0), (1, 1, 1000, 992))
+        pxc = cc.get_tile(0, (0, 0, 0, 0), (1, 1, 1000, 992))
+        pxc2 = cc.get_tile(1, (0, 0, 0, 0), (1, 1, 1000, 992))
+        try:
+            assert_array_equal(pxc, pxt)
+        except AssertionError:
+            assert_array_equal(pxc2, pxt)
+
+        pxt = tc.get_tile(0, (1, 0, 0, 0), (1, 1, 1000, 992))
+        pxc = cc.get_tile(0, (1, 0, 0, 0), (1, 1, 1000, 992))
+        pxc2 = cc.get_tile(1, (1, 0, 0, 0), (1, 1, 1000, 992))
+        try:
+            assert_array_equal(pxc, pxt)
+        except AssertionError:
+            assert_array_equal(pxc2, pxt)
+
+        pxt = tc.get_tile(0, (2, 0, 0, 0), (1, 1, 1000, 992))
+        pxc = cc.get_tile(0, (2, 0, 0, 0), (1, 1, 1000, 992))
+        pxc2 = cc.get_tile(1, (2, 0, 0, 0), (1, 1, 1000, 992))
+        try:
+            assert_array_equal(pxc, pxt)
+        except AssertionError:
+            assert_array_equal(pxc2, pxt)
