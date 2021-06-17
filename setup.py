d17b345678e8a67cc5bcb689443198b4193096ec
cmoehl <christoph.oliver.moehl@gmail.com>
2021-06-17 18:05:46 +0200

    update to version 0.2.0 in setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def readme():
 
 
 setup(name='yapic_io',
-      version='0.1.5',
+      version='0.2.0',
       description='io data handling module for various image sources as interface for pixel classification tools',
       long_description=readme(),
       author='Manuel Schoelling, Christoph Moehl',
